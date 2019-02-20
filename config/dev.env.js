f8f6f5db2e6eee4b5a0d87fe560caecc1b004a8a
Ridwyn <salihouridwyn@gmail.com>
2019-02-20 13:32:12 +0000

    Delete dev.env.js

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const merge = require("webpack-merge");
-const prodEnv = require("./prod.env");
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_KEY: '"98304107da83574cdac9140d458dce3d"'
-});
